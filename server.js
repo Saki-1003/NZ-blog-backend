3ab3ce30fb9a8459b930ac03bb73b8722bdf9d1e
Saki-1003 <saki.a19851003@gmail.com>
2024-11-04 07:53:51 +0900

    cors updated

--- server.js
+++ server.js
@@ -11,13 +11,7 @@ require("dotenv").config();
 
 const PORT = 5000;
 
-app.use(
-  cors({
-    origin: ["https://nz-blog-backend.onrender.com"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
